38932aa563c58ea223262c69c5af3fe01911b73c
Никита <nikitahunter23@gmail.com>
2022-10-13 16:25:57 +0300

    #17 Buttons load/save

---
+++ Assets/Scripts/Buildings/Building.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Building : BuildingsObject
+{
+    private int _levelOfHouse;
+    private int _residents;
+    private GameObject _model;
+    private Vector3 _position;
+    
+
+}
