ee5ffc3534a982397e02dc69a2fe71cf5168b86d
jassus213 <nikitahunter23@gmail.com>
2022-11-07 22:50:10 +0300

    #29 attach assets and make cursor mode

--- Assets/Scripts/Buildings/DestoryController.cs
+++ Assets/Scripts/Buildings/DestoryController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class DestoryController : MonoBehaviour
 {
 
+    [SerializeField] private Texture2D cursorDestroyer;
+    private GameObject _building;
     private bool isActive = false;
 
     [CanBeNull]
-    private GameObject IsHouse()
+    private GameObject GetGameObject()
     {
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hitInfo))
@@ -23,17 +25,36 @@ public class DestoryController : MonoBehaviour
 
     private void StartDestroy()
     {
-        var worldobject = IsHouse();
-        if (worldobject.GetComponent<IRemovable>() != null && Input.GetMouseButton(0))
+        var worldobject = GetGameObject();
+        _building = worldobject;
+        if (worldobject.GetComponent<IRemovable>() != null)
         {
-            worldobject.GetComponent<BuildingsObject>().Remove();
+            SetCustomCursor();
             Debug.Log(true);
+            if (Input.GetMouseButton(0))
+            {
+                worldobject.GetComponent<BuildingsObject>().Remove();
+                SetDefaultCursor();
+                Debug.Log(true);
+            }
+
         }
+        
     }
 
     public void StartFunc()
     {
         isActive = !isActive;
+    }
+
+    private void SetCustomCursor()
+    {
+        Cursor.SetCursor(cursorDestroyer, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    private void SetDefaultCursor()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
 
     private void Update()
